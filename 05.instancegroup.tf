044636a49f1906d1be670c801400e64d0716ebad
tsukaune <37860342+tsukaune@users.noreply.github.com>
2018-06-28 09:08:49 +0900

    Update 05.instancegroup.tf

--- 05.instancegroup.tf
+++ 05.instancegroup.tf
@@ -1,10 +1,10 @@
-resource "google_compute_instance_group" "tsukaune-tf-instance-group" {
-  name        = "tsukaune-tf-intstance-group"
-  description = "tsukaune-tf instance group"
+resource "google_compute_instance_group" "tf-instance-group" {
+  name        = "tf-intstance-group"
+  description = "tf instance group"
   zone        = "${var.region_zone}"
   instances = [
-    "${google_compute_instance.tsukaune-tf-web01.self_link}",
-    "${google_compute_instance.tsukaune-tf-web02.self_link}",
-    "${google_compute_instance.tsukaune-tf-web03.self_link}",
+    "${google_compute_instance.tf-web01.self_link}",
+    "${google_compute_instance.tf-web02.self_link}",
+    "${google_compute_instance.tf-web03.self_link}",
   ]
 }
