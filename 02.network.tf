4f5c079f5cb3754be8f03aab591f72c87d27afcd
tsukaune <37860342+tsukaune@users.noreply.github.com>
2018-06-28 09:05:11 +0900

    Update 02.network.tf

--- 02.network.tf
+++ 02.network.tf
@@ -1,26 +1,26 @@
-resource "google_compute_network" "tsukaune-tf-test-nw"{
-  name = "tsukaune-tf-test-nw"
+resource "google_compute_network" "tf-test-nw"{
+  name = "tf-test-nw"
   auto_create_subnetworks = "false"
 }
 
-resource "google_compute_subnetwork" "tsukaune-tf-test-subnet1"{
-  name          = "tsukaune-tf-test-subnet1"
+resource "google_compute_subnetwork" "tf-test-subnet1"{
+  name          = "tf-test-subnet1"
   ip_cidr_range = "10.200.11.0/24"
-  network       = "${google_compute_network.tsukaune-tf-test-nw.name}"
+  network       = "${google_compute_network.tf-test-nw.name}"
   description   = "tf-test-subnet1"
   region        = "${var.region}"
 }
-resource "google_compute_subnetwork" "tsukaune-tf-test-subnet2"{
-  name          = "tsukaune-tf-test-subnet2"
+resource "google_compute_subnetwork" "tf-test-subnet2"{
+  name          = "tf-test-subnet2"
   ip_cidr_range = "10.200.12.0/24"
-  network       = "${google_compute_network.tsukaune-tf-test-nw.name}"
+  network       = "${google_compute_network.tf-test-nw.name}"
   description   = "tf-test-subnet2"
   region        = "${var.region}"
 }
-resource "google_compute_subnetwork" "tsukaune-tf-test-subnet3"{
-  name          = "tsukaune-tf-test-subnet3"
+resource "google_compute_subnetwork" "tf-test-subnet3"{
+  name          = "tf-test-subnet3"
   ip_cidr_range = "10.200.13.0/24"
-  network       = "${google_compute_network.tsukaune-tf-test-nw.name}"
+  network       = "${google_compute_network.tf-test-nw.name}"
   description   = "tf-test-subnet3"
   region        = "${var.region}"
 }
